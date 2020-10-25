7e56dbaac497e8bde0ced214c94a984ce3ec1595
hernaval <hernavalasco@gmail.com>
2020-10-25 23:55:34 +0300

    start with package.json

--- index.js
+++ index.js
@@ -10,6 +10,24 @@ app.get("/",(req,res)=>{
     res.send("Hello word")
 })
 
+
+app.get("/webhook",(req,res)=>{
+    let VERIFY_TOKEN ="navalona123"
+
+    let mode = req.query['hub.mode']
+    let token = req.query['hub.verify_token']
+    let challenge = req.query['hub.challenge']
+
+    if(mode && token){
+        
+        if(mode ==="subscribe" && token === VERIFY_TOKEN){
+            console.log("WEBHOOK_VERIFIED")
+
+            res.status(200).send(challenge)
+        }
+    }
+})
+
 app.listen(process.env.PORT || 3000,()=>{
     console.log("app is running")
 })
