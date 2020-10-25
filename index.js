5eb29aa567bb0aba46c532158a586dd6e1d8debe
hernaval <hernavalasco@gmail.com>
2020-10-26 00:08:18 +0300

    see object

--- index.js
+++ index.js
@@ -28,6 +28,31 @@ app.get("/webhook",(req,res)=>{
     }
 })
 
+app.post("/webhook",(req,res)=>{
+    let body = req.body
+
+    console.log(body.object)
+    if(body.object==="page"){
+        
+        body.entry.forEach(entry=>{
+            let webhook_event = entry.messaging[0]
+            console.log("the webhook event is ",webhook_event)
+
+            let sender_psid = webhook_event.sender.id
+            console.log("Sender PSID ",sender_psid)
+
+            if(webhook_event.message){
+                console.log(webhook_event.message)
+            }else{
+                console.log(webhook_event.postback)
+            }
+        })
+        res.status(200).send("EVENT_RECEIVED")
+    }else{
+        res.sendStatus(404)
+    }
+})
+
 app.listen(process.env.PORT || 3000,()=>{
     console.log("app is running")
 })
